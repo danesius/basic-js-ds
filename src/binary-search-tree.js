efebb670f2d9e9f4ad2d49c9d9e498f924d23dee
Пользователь <denyskropache@gmail.com>
2023-04-17 18:35:22 +0300

    feat half

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,119 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+// const { nodenew } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
-* using Node from extensions
+* using nodenew from extensions
 */
+class nodenew {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.rootnodenew = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() { return this.rootnodenew; }
+
+  add(data) {
+    const newnodenew = new nodenew(data);
+    if (this.rootnodenew === null) {
+      this.rootnodenew = newnodenew;
+    } else {
+      this.insertnodenew(this.rootnodenew, newnodenew);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertnodenew(nodenew, newnodenew) {
+    if (newnodenew.data < nodenew.data) {
+      if (nodenew.left === null) {
+        nodenew.left = newnodenew;
+      } else {
+        this.insertnodenew(nodenew.left, newnodenew);
+      }
+    } else {
+      if (nodenew.right === null) {
+        nodenew.right = newnodenew;
+      } else {
+        this.insertnodenew(nodenew.right, newnodenew);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) { return this.searchnodenew(this.rootnodenew, data) !== null; }
+
+  find(data) { return this.searchnodenew(this.rootnodenew, data); }
+
+  searchnodenew(nodenew, data) {
+    if (nodenew === null) {
+      return null;
+    } else if (data < nodenew.data) {
+      return this.searchnodenew(nodenew.left, data);
+    } else if (data > nodenew.data) {
+      return this.searchnodenew(nodenew.right, data);
+    } else {
+      return nodenew;
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) { this.rootnodenew = this.removenodenew(this.rootnodenew, data); }
+
+  removenodenew(nodenew, key) {
+    if (nodenew === null) {
+      return null;
+    } else if (key < nodenew.data) {
+      nodenew.left = this.removenodenew(nodenew.left, key); return nodenew;
+    } else if (key > nodenew.data) {
+      nodenew.right = this.removenodenew(nodenew.right, key); return nodenew;
+    } else {
+      if (nodenew.left === null && nodenew.right === null) {
+        nodenew = null; return nodenew;
+      }
+      if (nodenew.left === null) {
+        nodenew = nodenew.right; return nodenew;
+      } else if (nodenew.right === null) {
+        nodenew = nodenew.left; return nodenew;
+      }
+      const aux = this.findMinnodenew(nodenew.right);
+      nodenew.data = aux.data;
+      nodenew.right = this.removenodenew(nodenew.right, aux.data); return nodenew;
+    }
+  }
+
+  findMinnodenew(nodenew) {
+    if (nodenew.left === null) {
+      return nodenew;
+    } else {
+      return this.findMinnodenew(nodenew.left);
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootnodenew === null) {
+      return null;
+    }
+    let currentnodenew = this.rootnodenew;
+    while (currentnodenew.left !== null) {
+      currentnodenew = currentnodenew.left;
+    }
+    return currentnodenew.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootnodenew === null) {
+      return null;
+    }
+    let currentnodenew = this.rootnodenew;
+    while (currentnodenew.right !== null) {
+      currentnodenew = currentnodenew.right;
+    }
+    return currentnodenew.data;
   }
 }
 
